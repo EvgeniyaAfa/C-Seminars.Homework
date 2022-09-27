b4981608ab8ad3787ae53bb10a8055f44cfaf839
EvgeniyaAfa <evgeniya_afanasieva@mail.ru>
2022-09-28 00:29:35 +0300

    HW Les 9

--- Homework_Les9/Program.cs
+++ Homework_Les9/Program.cs
@@ -1,62 +1,107 @@
-﻿int[,,] CreateRandom3dArray(int rows, int columns, int sheet)
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+/*
+void Shownums(int n)
 {
-    int[,,] array = new int[rows, columns, sheet];
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < columns; j++)
-        {
-            int k = 0;
-            while (k < sheet)
-            {
-                int number = new Random().Next(10, 100);
-                if (FindNum(array, number)) continue;
-                array[i, j, k] = number;
-                k++;
-            }
-        }
-    return array;
-
+    if (n > 1) Shownums(n - 1);
+    Console.Write(n + " ");
 }
 
-bool FindNum(int[,,] array, int number)
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Shownums(num);
+
+
+
+//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+//5683
+
+int SumofDig (int n)
 {
-    bool find = false;
-    for (int i = 0; i < array.GetLength(0); i++)
-        for (int j = 0; j < array.GetLength(1); j++)
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                if (array[i, j, k] == number) find = true;
-
-            }
-    return find;
-
-
+    if (n > 0)  return SumofDig(n / 10) + n % 10;
+    else return 0;
 }
 
-void ShowIndex(int[,,] array)
+Console.Write(SumofDig(5683));
+
+
+//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void Shownums(int m, int n)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    if (n > m) Shownums(m, n - 1);
+    
+    Console.Write(n + " ");
+    
+}
+
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Shownums(num, n1);
+
+
+
+
+//Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+int ReturnA (int a, int b)
+{
+    if (b > 1) return ReturnA (a, b - 1 ) * a;
+    else return a;
+}
+Console.Write(ReturnA(2 ,2));
+
+
+//_________________________HW_____________________________________
+
+//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int ShowSum (int m, int n)
+{
+    if (n > m ) return ShowSum (m, n - 1) + n;
+        else return m;
+}
+Console.Write("Input 1 number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 1 number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(ShowSum(num1,num2));
+
+
+//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 29
+
+int AnkermanFun (int m, int n)
+{
+    if (m == 0) 
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                Console.WriteLine($"{ array[i, j, k]} ({ i},{ j},{ k})");
-            }
-            Console.WriteLine();
-        }
+        return n + 1;
+    }
+
+    else if (n == 0 && m > 0)
+    {
+        return AnkermanFun (m - 1, 1);
+    } 
+
+    else  
+    {
+        return AnkermanFun ( m - 1, AnkermanFun (m, n -1 ));
+
     }
 }
 
-Console.Write("Input rows number: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input columns number: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input quantity of sheet: ");
-int sheet = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-int[,,] myarray = CreateRandom3dArray(rows, columns, sheet);
-ShowIndex(myarray);
 
-
-
-
+Console.Write("Input 1 number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 1 number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{AnkermanFun(num1, num2)}");
+*/
